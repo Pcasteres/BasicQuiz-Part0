dc31bee5f6afe41e92cf22a043f565e1d37aa6fa
Paula <pcasteres@hotmail.com>
2022-02-14 12:17:56 +0000

    Intentando desactivar botones

--- app/src/main/java/es/ulpgc/eite/da/basicquizlab/MainActivity.java
+++ app/src/main/java/es/ulpgc/eite/da/basicquizlab/MainActivity.java
@@ -107,24 +107,7 @@ public class MainActivity extends AppCompatActivity {
     replyText.setText(R.string.empty_text);
   }
   public void enableButtons (){
-    trueButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
 
-      }
-    });
-    falseButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-
-      }
-    });
-    nextButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-
-      }
-    });
   }
 
 
